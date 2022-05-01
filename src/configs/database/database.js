4bdc0e4bed056dd407763a8785cda48938179010
joaovmvale <joao.marquesvale@gmail.com>
2022-04-30 22:13:44 -0300

    [BACKEND-03] - Login endpoint finished and some general settings

--- src/configs/database/database.js
+++ src/configs/database/database.js
@@ -1,14 +1,25 @@
 const Sequelize = require('sequelize');
 const credentials = require('./credentials');
+require('dotenv').config();
+
+const env = process.env.ENVIRONMENT;
+var environment;
+
+if (env === 'LOCAL') {
+  environment = "development";
+} else if (env === 'UNIVERSITY') {
+  environment = "university";
+}
+
 
 const sequelize = new Sequelize(
-  credentials['university']['database'],
-  credentials['university']['username'],
-  credentials['university']['password'],
+  credentials[environment]['database'],
+  credentials[environment]['username'],
+  credentials[environment]['password'],
   {
-    host: credentials['university']['host'],
-    port: credentials['university']['port'],
-    dialect: credentials['university']['dialect'],
+    host: credentials[environment]['host'],
+    port: credentials[environment]['port'],
+    dialect: credentials[environment]['dialect'],
   }
 );
 
